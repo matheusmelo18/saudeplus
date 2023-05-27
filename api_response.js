0d419723f3bd25d8134f6f32d8a30c45101633ab
StefanoDPCarraro <stefanodpc29@gmail.com>
2023-05-27 13:59:51 -0300

    Versao final bruta front

--- api_response.js
+++ api_response.js
@@ -1,20 +1,16 @@
 
-$.getJSON('https://random.dog/woof.json', function(data) {
-var answer = data
-});
-
-
   function submitForm(event) {
     var myForm = document.getElementById("form");
     var result = document.getElementById("result");
     var inputValue = document.getElementById('problem').value
-    console.log(myForm)
 
       event.preventDefault();
       myForm.submit();
       result.innerHTML = `${inputValue}`
 
-      $.getJSON('https://random.dog/woof.json', function(data) { // pegar o inputValue e enviar pra api 
-        console.log(inputValue);
+      $.getJSON('https://random.dog/woof.json', function(data) {
+            var answers = document.getElementById("answers");
+
+            answers.innerHTML = `${data}`
         });
   }  
