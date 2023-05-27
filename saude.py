bbefb992bf77407651edc4c6ca1909cc27f32d22
ArthurBoth <arthurboth.6@gmail.com>
2023-05-27 13:28:02 -0300

    Backend básico funcionando

--- saude.py
+++ saude.py
@@ -6,17 +6,25 @@ headers = {
     "Content-Type": "application/json"
 }
 
+mensagem1 = "Tenho os seguintes sintomas: "
+mensagem2 = "eu to com uma tosse danada e dores de barriga aleatórias"
+mensagem3 = ", qual seria o melhor tipo de especialisa que eu deveira procurar para resolver os meus problemas?"
+
+
+
 data = {
     "model": "gpt-3.5-turbo",  # O modelo que você deseja usar
     "messages": [
-        {"role": "system", "content": "Você é um assistente de bate-papo"},
-        {"role": "user", "content": "Qual é a sua cor favorita?"},
-        {"role": "user", "content": "Tenho os seguintes sintomas: Presença de cravos e espinhas no rosto, costas e peito, inflamação da pele, oleosidade excessiva.Qual médico é o mais recomendado para tratamento?"}
+        {"role": "user", "content": mensagem1 + mensagem2 + mensagem3}
     ]
 }
 
 url = "https://api.openai.com/v1/chat/completions"
 
 response = requests.post(url, headers=headers, data=json.dumps(data))
-print(response.json())
 
+resposta = json.loads(response.text)
+
+mensagem = resposta['choices'][0]['message']['content']
+
+print(mensagem)
